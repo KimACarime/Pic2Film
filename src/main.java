98745ee0a33cb4076550271fa655ac7d0df9047d
KimACarime <kcarime@gmail.com>
2015-06-28 16:55:58 +0200

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Hcink on 27/06/2015.
+ */
+import java.lang.*;
+import java.io.*;
+import java.*;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("coucou le monde");
+    }
+}
