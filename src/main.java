4a1e31980850a8526545b5a5d7faa9cd6274ffa2
KimACarime <kcarime@gmail.com>
2015-06-28 17:06:20 +0200

    test 1er push

--- src/main.java
+++ src/main.java
@@ -2,11 +2,12 @@
  * Created by Hcink on 27/06/2015.
  */
 import java.lang.*;
-import java.io.*;
-import java.*;
+
 
 public class main {
     public static void main(String[] args) {
         System.out.println("coucou le monde");
+        System.out.println("chargement des images");
+        System.out.println("creation de la video");
     }
 }
